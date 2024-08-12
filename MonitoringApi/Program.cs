ce1f8b44e185e4cc184a0ee1911946f41e8503ef
AshJPeek <a.j.peek97@gmail.com>
2024-08-12 14:49:49 +0100

    Implemented basic api health checks using a random simulated response time.

--- MonitoringApi/Program.cs
+++ MonitoringApi/Program.cs
@@ -1,9 +1,24 @@
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using MonitoringApi.HealthChecks;
+
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHealthChecks()
+    .AddCheck<RandomHealthCheck>("Site Health Check")
+    .AddCheck<RandomHealthCheck>("Database Health Check");
+
+builder.Services.AddHealthChecksUI(opts =>
+{
+    opts.AddHealthCheckEndpoint("api", "/health");
+    opts.SetEvaluationTimeInSeconds(5);
+    opts.SetMinimumSecondsBetweenFailureNotifications(10);
+}).AddInMemoryStorage();
+
 
 var app = builder.Build();
 
@@ -18,5 +33,10 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
+app.MapHealthChecksUI();
 
 app.Run();
